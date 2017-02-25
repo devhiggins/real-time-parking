703122cdaf0609584a302c113678e0a5385d28d9
devhiggins <devinhiggins13@gmail.com>
2017-02-25 12:17:25 -0500

    Add files via upload

--- MARTA.py
+++ MARTA.py
@@ -2,32 +2,37 @@ from time import sleep
 import RPi.GPIO as GPIO
 import requests
 
+
 GPIO.setmode(GPIO.BOARD)
 
-# button positions on the red board
-# button 1 is car entering
-# button 2 is car exiting
-button1 = 16
-button2 = 12
+# button positions used on the board
+# buttonA is entering cars
+# buttonB is exiting cars
+buttonA = 16
+buttonB = 12
 
-GPIO.setup(button1,GPIO.IN,pull_up_down=GPIO.PUD_UP)
-GPIO.setup(button2,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+GPIO.setup(buttonA, GPIO.IN , pull_up_down = GPIO.PUD_UP)
+GPIO.setup(buttonB, GPIO.IN , pull_up_down = GPIO.PUD_UP)
 
-# initialize lots
-payload = {"North Springs":500}
-requests.post('http://localhost:8080/api/v1/makeLot', json = payload)
+payload = {"name":"North Springs","totalSpaces":2378}
+requests.post("http://192.168.0.58:8080/api/v1/makeLot", json = payload)
 
-# while we want to collect data
-while(true):
+# while we want to receive data 
+while(True):
 
-        if GPIO.input(button1) == 0:
-                payload = {"North Springs": 500}
-                requests.post('http://localhost:8080/api/v1/increment', json = payload)
-                sleep(.1)
-                # send increment to server
-        
-        if GPIO.input(button2) == 0:
-                payload = {"North Springs":500}
-                # send decrement to server
-                requests.post('http://localhost:8080/api/v1/decrement', json = payload)
-                sleep(.1)
+    if GPIO.input(buttonA) == 0:
+        # send increment to server
+        #payload values?
+        payload = {"name":"North Springs"}
+        requests.post("http://192.168.0.58:8080/api/v1/increment", json = payload)
+        sleep(.1)
+
+    if GPIO.input(buttonB) == 0:
+        # send decrement to server
+        #payload values?
+        payload = {"name":"North Springs"}
+        requests.post("http://192.168.0.58:8080/api/v1/decrement", json = payload)
+        sleep(.1)
+
+    
+    
---
+++ database_Count.py
@@ -0,0 +1,39 @@
+import RPi.GPIO as GPIO
+import sqlite3
+import time, datetime
+
+current_time = "2017-02-25 11:35:47"
+GPIO.setmode(GPIO.BOARD)
+
+# button positions used on the board
+# buttonA is entering cars
+# buttonB is exiting cars
+buttonA = 16
+buttonB = 12
+
+GPIO.setup(buttonA, GPIO.IN , pull_up_down = GPIO.PUD_UP)
+GPIO.setup(buttonB, GPIO.IN , pull_up_down = GPIO.PUD_UP)
+conn = sqlite3.connect("MARTA.db")
+conn.commit()
+c = conn.cursor()
+c.execute('''CREATE TABLE parking_info(name text, open_spaces REAL)''')
+c.execute('''INSERT INTO parking_info VALUES('North Springs', 500)''')
+
+
+# while we want to receive data 
+while(True):
+
+    if GPIO.input(buttonA) == 0:
+        c.execute('''UPDATE parking_info SET open_spaces = open_spaces + 1''')
+        c.execute("SELECT * FROM parking_info")
+        print(c.fetchone())
+        time.sleep(1)
+
+    if GPIO.input(buttonB) == 0:
+        c.execute('''UPDATE parking_info SET open_spaces = open_spaces - 1''')
+        c.execute("SELECT * FROM parking_info")
+        print(c.fetchone())
+        time.sleep(1)
+
+    
+    
